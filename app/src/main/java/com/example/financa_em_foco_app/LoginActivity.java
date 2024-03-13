529d8ebc50643f99290e9d480d6df515edf83015
limaenz <enzolimafranca@gmail.com>
2024-03-12 23:20:41 -0300

    feat: adding news screens login and register

---
+++ app/src/main/java/com/example/financa_em_foco_app/LoginActivity.java
@@ -0,0 +1,22 @@
+package com.financa_em_foco_app;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+public class LoginActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle("");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        toolbar.setNavigationOnClickListener(v -> getOnBackPressedDispatcher().onBackPressed());
+    }
+}
