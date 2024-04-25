db9f73209dbbafdec864b94ffab164b7bde5d819
limaenz <enzolimafranca@gmail.com>
2024-04-25 00:39:24 -0300

    feat: adding pie chart and functions

--- app/src/main/java/com/example/financa_em_foco_app/Fragments/ControleFinanceiroFragment.java
+++ app/src/main/java/com/example/financa_em_foco_app/Fragments/ControleFinanceiroFragment.java
@@ -2,6 +2,7 @@ package com.example.financa_em_foco_app.Fragments;
 
 import android.app.AlertDialog;
 import android.graphics.Color;
+import android.graphics.Typeface;
 import android.os.Bundle;
 
 import androidx.annotation.NonNull;
@@ -18,15 +19,18 @@ import com.example.financa_em_foco_app.Fragments.Dialogs.DespesasDialogFragment;
 import com.example.financa_em_foco_app.Models.Despesa;
 import com.example.financa_em_foco_app.R;
 import com.example.financa_em_foco_app.databinding.FragmentControleFinanceiroBinding;
+import com.github.mikephil.charting.charts.PieChart;
+import com.github.mikephil.charting.components.Legend;
+import com.github.mikephil.charting.data.PieData;
+import com.github.mikephil.charting.data.PieDataSet;
+import com.github.mikephil.charting.data.PieEntry;
+import com.github.mikephil.charting.formatter.PercentFormatter;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
-import org.eazegraph.lib.charts.PieChart;
-import org.eazegraph.lib.models.PieModel;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -54,15 +58,6 @@ public class ControleFinanceiroFragment extends Fragment {
         adapter = new DespesaAdapter(getContext(), despesas);
         recyclerView.setAdapter(adapter);
         carregarTransacoes();
-
-        PieChart mPieChart = (PieChart) binding.piechart;
-
-        mPieChart.addPieSlice(new PieModel("Freetime", 15, Color.parseColor("#FE6DA8")));
-        mPieChart.addPieSlice(new PieModel("Sleep", 25, Color.parseColor("#56B7F1")));
-        mPieChart.addPieSlice(new PieModel("Work", 35, Color.parseColor("#CDA67F")));
-        mPieChart.addPieSlice(new PieModel("Eating", 9, Color.parseColor("#FED70E")));
-
-        mPieChart.startAnimation();
     }
 
     private void mostrarModal() {
@@ -91,17 +86,62 @@ public class ControleFinanceiroFragment extends Fragment {
             public void onDataChange(@NonNull DataSnapshot snapshot) {
                 despesas.clear();
 
+                float valorTotalGasto = 0;
+                float valorTotalGanho = 0;
+
                 for (DataSnapshot itemSnapshot : snapshot.getChildren()) {
                     Despesa transacao = itemSnapshot.getValue(Despesa.class);
                     despesas.add(transacao);
+
+                    if (transacao.tipo.equals("Gasto")) {
+                        valorTotalGasto += transacao.valor;
+                    } else if (transacao.tipo.equals("Ganho")) {
+                        valorTotalGanho += transacao.valor;
+                    }
                 }
 
-                if (despesas.isEmpty()) binding.textViewNaoPossuiDespesas.setVisibility(View.VISIBLE);
+                if (despesas.isEmpty())
+                    binding.textViewNaoPossuiDespesas.setVisibility(View.VISIBLE);
 
                 if (!despesas.isEmpty()) {
                     binding.piechart.setVisibility(View.VISIBLE);
                     binding.textViewNaoPossuiDespesas.setVisibility(View.INVISIBLE);
                 }
+
+                PieChart mPieChart = binding.piechart;
+                mPieChart.clear();
+
+                ArrayList<PieEntry> entries = new ArrayList<>();
+                entries.add(new PieEntry(valorTotalGasto, "Gasto"));
+                entries.add(new PieEntry(valorTotalGanho, "Ganho"));
+
+                PieDataSet dataSet = new PieDataSet(entries, "");
+                dataSet.setColors(Color.parseColor("#AD88C6"), Color.parseColor("#7469B6"));
+                dataSet.setValueTextSize(13f);
+                dataSet.setValueTextColor(Color.WHITE);
+                dataSet.setValueTypeface(Typeface.DEFAULT_BOLD);
+
+                PieData data = new PieData(dataSet);
+                data.setValueFormatter(new PercentFormatter(binding.piechart));
+
+                mPieChart.setData(data);
+                mPieChart.getDescription().setEnabled(false);
+                mPieChart.setUsePercentValues(true);
+                mPieChart.setDrawEntryLabels(false);
+                mPieChart.setExtraOffsets(5, 10, 5, 10);
+                mPieChart.animateY(1000);
+
+                Legend legend = binding.piechart.getLegend();
+                legend.setHorizontalAlignment(Legend.LegendHorizontalAlignment.CENTER);
+                legend.setOrientation(Legend.LegendOrientation.HORIZONTAL);
+                legend.setTextSize(16f);
+                legend.setTextColor(Color.BLACK);
+                legend.setForm(Legend.LegendForm.CIRCLE);
+                legend.setTypeface(Typeface.DEFAULT_BOLD);
+                legend.setXEntrySpace(25f);
+                legend.setYOffset(30f);
+
+                mPieChart.invalidate();
 
                 if (despesas.size() == 4) binding.textViewVerMais.setVisibility(View.VISIBLE);
 
